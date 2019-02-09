cf27309d976a3af830e45c73951f67237220fcc1
Steven Blair <blair.steven@outlook.com>
2019-02-08 21:59:20 -0600

    Initial Commit

---
+++ TakeANumber/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TakeANumber
+//
+//  Created by Steven Blair on 2/8/19.
+//  Copyright © 2019 Steven Blair. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
